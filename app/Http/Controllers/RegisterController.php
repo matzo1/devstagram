ff07da73016a838dded943e6fe11acf9e228185a
matzo <maxi.santa.2@gmail.com>
2024-03-23 16:04:35 -0300

    12.Finalizando el registro de usuario

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,13 +16,25 @@ class RegisterController extends Controller
         //dd($request);
         //dd($request->get('username'));
 
+        //Modificar el Request
+        $request->request->add(['username' => Str::slug($request->username)]);
+
         //Validacion
         $this->validate($request, [
             'name' => 'required|max:30',
             'username' => 'required|unique:users|min:3|max:30',
             'email' => 'required|unique:users|email|max:100',
-            'password' => 'required',
+            'password' => 'required|confirmed|min:6',
         ]);
 
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        //redireccionar
+        return redirect()->route('posts.index');
     }
 }
